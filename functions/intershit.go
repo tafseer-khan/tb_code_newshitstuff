698e10aea0dc89f25f23dbcdb904f04776821170
tafseer-khan <tafseer.k.khan@gmail.com>
2023-02-03 13:27:52 -0600

    asdf

--- functions/intershit.go
+++ functions/intershit.go
@@ -8,12 +8,12 @@ import (
 )
 
 //go:wasm-module http/68747470733a2f2f6769746875622e636f6d2f746166736565722d6b68616e2f746573745f73747566662f626c6f622f6d61696e2f68656c6c6f5f7761736d2e776173692e7761736d3f7261773d74727565
-//export doStuff
-func doStuff(x event.Event) uint32
+//export do_stuff
+func do_stuff(x event.Event) uint32
 
-//export memView1
+//export doShit
 func doShit(e event.Event) uint32 {
-	mvId := doStuff(e)
+	mvId := do_stuff(e)
 	mv, err := i2mv.Open(mvId)
 	if err != nil {
 		return 1

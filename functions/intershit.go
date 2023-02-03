cbba6f0f268538eedaa020c33167314d2c4da55c
tafseer-khan <tafseer.k.khan@gmail.com>
2023-02-03 13:25:39 -0600

    asdf

--- functions/intershit.go
+++ functions/intershit.go
@@ -1,8 +1,17 @@
+package lib
+
+import (
+	"io/ioutil"
+
+	"github.com/taubyte/go-sdk/event"
+	"github.com/taubyte/go-sdk/i2mv"
+)
+
 //go:wasm-module http/68747470733a2f2f6769746875622e636f6d2f746166736565722d6b68616e2f746573745f73747566662f626c6f622f6d61696e2f68656c6c6f5f7761736d2e776173692e7761736d3f7261773d74727565
 //export doStuff
 func doStuff(x event.Event) uint32
 
-//export doShit
+//export memView1
 func doShit(e event.Event) uint32 {
 	mvId := doStuff(e)
 	mv, err := i2mv.Open(mvId)
